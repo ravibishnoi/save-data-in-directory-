144a97f0e89b1e53c0a4a8cc507f95496fc94c65
ravibishnoi <rbk4997@gmail.com>
2020-04-14 19:18:39 +0530

    Ravi First intial commit

---
+++ Data save using URL/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Data save using URL
+//
+//  Created by AshutoshD on 14/04/20.
+//  Copyright © 2020 ravindraB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        print("Hello World!")
+    }
+
+
+}
+
